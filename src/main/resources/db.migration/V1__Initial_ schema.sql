76fe5c843e44c4963f73f51faecc806a584a2b03
ogurcan1 <ozgur.gurcan@pia-team.com>
2023-02-07 15:54:08 +0300

    db config and location table init

--- src/main/resources/db.migration/V1__Initial_ schema.sql
+++ src/main/resources/db.migration/V1__Initial_ schema.sql
@@ -1,9 +1,16 @@
 CREATE TABLE location
 (
-    id           PRIMARY KE   NOT NULL,
+    id           SERIAL PRIMARY KEY NOT NULL,
     city         varchar(255) NOT NULL,
     district     varchar(255) NOT NULL,
     town         varchar(255) NOT NULL,
     neighborhood varchar(255) NOT NULL,
-    zip_code     varchar(255) NOT NULL,
+    zip_code     varchar(255) NOT NULL
 );
+
+
+
+-- COPY location(city,district,town,neighborhood,zip_code)
+--     FROM 'C:\Program Files\PostgreSQL\15\scripts\location.csv'
+--     DELIMITER ';'
+--     CSV HEADER;
